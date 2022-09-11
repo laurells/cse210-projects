79c1853fa4e7f227512412453ebdd25716daacc7
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-11 17:49:47 +0100

    My first c# program.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,13 @@ namespace Prep1
             Console.WriteLine("This is prep 1");
 
             // Write your code here
+            Console.Write("What is your first name? ");
+            string name1 = Console.ReadLine();
+            Console.Write("What is your last name? ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine($"Your name is {name2}, {name1} {name2}.");
+
+
         }
     }
 }

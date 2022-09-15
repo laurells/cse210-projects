8d88423839d968507ec6ad5f942746c195bf54f4
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-15 17:07:04 +0100

    A guessing game program in  C#

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,8 +5,37 @@ namespace Prep3
     class Program
     {
         static void Main(string[] args)
+
         {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
+
+            int guess = -1
+
             Console.WriteLine("This is Prep 3");
+
+
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if(magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it right!");
+                }
+            }
+
+           
         }
     }
 }

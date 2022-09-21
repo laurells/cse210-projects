cf64634eed1b9b72fe4aa42fbd40fa716d682a8a
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-21 11:41:15 +0100

    C# funtion basics

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,7 +7,36 @@ namespace Prep4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 4");
+            List<int> numbers = new List<int>();
+
+            int userNumber = -1;
+
+            while (userNumber != 0)
+            {
+                Console.Write("Enter a number (0 to quit): ");
+
+                string userResponse = Console.ReadLine();
+                userNumber = int.Parse(userResponse);
+
+                numbers.Add(userNumber);
+                if(userNumber == 0){
+                break;
+                }
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = sum/ numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            
         }
+    
     }
 }

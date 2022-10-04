27d7a220c70543cf86c95f78b65eb266320e7de8
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-10-04 14:44:43 +0100

    changes

--- developer/Unit02/Program.cs
+++ developer/Unit02/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using Unit02.Game;
 
-namespace Unit02
+namespace Unit02;
+
+/// <summary>
+/// The program's entry point.
+/// </summary>
+class Program
 {
-    class Program
+    ///Begins the Hi-Lo game by calling the Director class
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Director director = new Director();
+        director.StartGame();
+        return 0;
     }
 }
